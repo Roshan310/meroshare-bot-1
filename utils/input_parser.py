69d15da8d237c39653de825a268fb66af552fafc
Roshan Aryal <64967304+Roshan310@users.noreply.github.com>
2023-02-10 22:09:02 +0545

    use list comprehension to create the list

--- utils/input_parser.py
+++ utils/input_parser.py
@@ -1,8 +1,6 @@
 
 def input_parser(file):
-    'Takes the dict from of each lines, breaks them into another list of values seaprated by comma'
-    i = []
-    for item in file:
-        i.append(item)
+    'Takes the dict from each lines, breaks them into another list of values seaprated by comma'
+    i = [item for item in file]
     return i
         
